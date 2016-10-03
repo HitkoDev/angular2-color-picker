e0eac48288036a83bebbebecbeea4f5a528dd2a3
Marko Novak <marko.novak@erpium.com>
2016-10-03 10:38:55 +0200

    Explicit export

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 "use strict";
-function __export(m) {
-    for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
-}
-__export(require('./app/color-picker/color-picker.service'));
-__export(require('./app/color-picker/color-picker.directive'));
-__export(require('./app/color-picker/color-picker.class'));
-__export(require('./app/color-picker/color-picker.module'));
+var color_picker_service_1 = require('./app/color-picker/color-picker.service');
+exports.ColorPickerService = color_picker_service_1.ColorPickerService;
+var color_picker_directive_1 = require('./app/color-picker/color-picker.directive');
+exports.ColorPickerDirective = color_picker_directive_1.ColorPickerDirective;
+exports.TextDirective = color_picker_directive_1.TextDirective;
+exports.SliderDirective = color_picker_directive_1.SliderDirective;
+exports.DialogComponent = color_picker_directive_1.DialogComponent;
+var color_picker_class_1 = require('./app/color-picker/color-picker.class');
+exports.ColorPickerOptions = color_picker_class_1.ColorPickerOptions;
+exports.Hsla = color_picker_class_1.Hsla;
+exports.Hsva = color_picker_class_1.Hsva;
+exports.Rgba = color_picker_class_1.Rgba;
+exports.SliderDimension = color_picker_class_1.SliderDimension;
+exports.SliderPosition = color_picker_class_1.SliderPosition;
+var color_picker_module_1 = require('./app/color-picker/color-picker.module');
+exports.ColorPickerModule = color_picker_module_1.ColorPickerModule;
 //# sourceMappingURL=index.js.map
--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-export * from './app/color-picker/color-picker.service'
-export * from './app/color-picker/color-picker.directive'
-export * from './app/color-picker/color-picker.class'
-export * from './app/color-picker/color-picker.module'
+export { ColorPickerService } from './app/color-picker/color-picker.service'
+export { ColorPickerDirective, TextDirective, SliderDirective, DialogComponent } from './app/color-picker/color-picker.directive'
+export { ColorPickerOptions, Hsla, Hsva, Rgba, SliderDimension, SliderPosition } from './app/color-picker/color-picker.class'
+export { ColorPickerModule } from './app/color-picker/color-picker.module'
